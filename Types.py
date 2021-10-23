8589258b76f6bb469781e0ed13042a2d35a38238
aiooq <59270390+aiooq@users.noreply.github.com>
2021-10-23 21:15:20 +0700

    Done task #1

--- Types.py
+++ Types.py
@@ -6,27 +6,27 @@ def main(tuple):
             i+=1
             continue
 
-        if not "in" in tuple[i]:
-            i+=1
-            continue
-        
         out = None
-        if "out" in tuple[i]:
-            out = tuple[i]["out"]
+        value = None
+        if "in" in tuple[i]:    
+            if "out" in tuple[i]:
+                out = tuple[i]["out"]
 
-        value = input(tuple[i]["in"])
+            value = input(tuple[i]["in"])
 
-        if "type" in tuple[i]:
-            try:
-                if tuple[i]["type"] == str and value.isdigit():
-                    raise Exception()
-                elif tuple[i]["type"] == float and value.isdecimal():
-                    raise Exception()
-                else:
-                    value=tuple[i]["type"](value)
-            except:
-                print("Некорректное значение, ожидается {0}, пожалуйста повторите ввод!".format(tuple[i]["type"]))
-                continue
+            if "type" in tuple[i]:
+                try:
+                    if tuple[i]["type"] == str:
+                        if value.isdigit():
+                            raise Exception()
+                    elif tuple[i]["type"] == float:
+                        if value.isdecimal():
+                            raise Exception()
+                    else:
+                        value=tuple[i]["type"](value)
+                except:
+                    print("Некорректное значение, ожидается {0}, пожалуйста повторите ввод!".format(tuple[i]["type"]))
+                    continue
 
         try:
             value = tuple[i]["def"](value,out)
@@ -41,10 +41,39 @@ def main(tuple):
                 print("Некорректное значение, ожидается число, пожалуйста повторите ввод!")
             elif e.args[0]=="StrFormatIsNotValid":
                 i-=1
-                print("Некорректный формат, пожалуйста смотрите пример и повторите ввод!")                
+                print("Некорректный формат, пожалуйста смотрите пример и повторите ввод!") 
+            else:
+                print("Ошибка в конфигурации: "+e)               
             continue
         finally:
             i+=1
+
+def ForTask1(value, out):
+    values=[
+        True,
+        int(5),
+        float(1.2),
+        complex(3.14 - 0j),   
+        str("stirng"),
+        bytes(25),
+        bytearray([25,50]),
+        list([1,2,3]),
+        tuple((1,2,3)),
+        set({3,1,2}),
+        frozenset({3,1,2}),
+        dict({"key":"value"})]
+
+    types=[
+        bool,int,float,complex,str,bytes,bytearray,list,tuple,set,frozenset,dict
+    ]
+
+    for i in range(len(values)):
+        print("Значение {0}, тип: {1}, соответсвие: {2}".format(
+            values[i],
+            type(values[i]), 
+            isinstance(values[i], types[i])))
+
+    return None
 
 def ForTask5(value, out):
     if(value<0):
@@ -60,7 +89,8 @@ def ForTask5(value, out):
 # Если необходимо, то список можно сортировать, так как номера задач весьма условны
 # Сортировка внутри кортежа (одной задачи), недопустима!
 tasks = list()
-tasks.append(({"in":"Введите натуральное число (новый элемент рейтинга): ", "out":"Результат = {0}", "def":ForTask5, "type": int})) 
+tasks.append(({"def":ForTask1}))
+#tasks.append(({"in":"Введите натуральное число (новый элемент рейтинга): ", "out":"Результат = {0}", "def":ForTask5, "type": int}))
 
 # Основной цикл
 while True:
