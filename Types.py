b71720327b942c8f47dbcf83127f44320c065734
aiooq <59270390+aiooq@users.noreply.github.com>
2021-10-25 10:00:47 +0700

    Done
    Improved program configuration, the ability to check several types from str that the user entered
    
    Versatility of the code for any task, all checks are included in the program, if the task configuration is set correctly
    
    The principle of modularity within the program:
    1 task = 1 function
    1 sub-task = 1 function
    
    Улучшенное конфигурирование программы, возможность проверки нескольких типов из str, которые ввел пользователь
    
    Универсальность кода под любые задачи, все проверки входят в состав программы, если корректно задать конфигурацию задач
    
    Принцип модульности внутри программы:
    1 задача = 1 функция
    1 подзадача = 1 функция

--- Types.py
+++ Types.py
@@ -8,7 +8,7 @@ def main(tuple):
 
         out = None
         value = None
-        if "in" in tuple[i]:    
+        if "in" in tuple[i]:  
             if "out" in tuple[i]:
                 out = tuple[i]["out"]
 
@@ -16,20 +16,42 @@ def main(tuple):
 
             if "type" in tuple[i]:
                 try:
-                    if tuple[i]["type"] == str:
-                        if value.isdigit():
-                            raise Exception()
-                    elif tuple[i]["type"] == float:
-                        if value.isdecimal():
-                            raise Exception()
-                    else:
-                        value=tuple[i]["type"](value)
+                    type_value=tuple[i]["type"]
+                    if type(type_value)==type:
+                        type_value={type_value}
+                    
+                    result = False
+                    for item in type_value:
+                        try:
+                            if item == str:
+                                try:
+                                    float(value)
+                                    continue
+                                except ValueError:
+                                    if value.isnumeric():
+                                        continue
+
+                            elif item == int:
+                                if not value.isnumeric():
+                                    continue
+                            else:
+                                value=item(value)
+                            result = True
+                            break
+                        except:
+                            continue
+
+                    if not result:
+                        raise Exception()
                 except:
                     print("Некорректное значение, ожидается {0}, пожалуйста повторите ввод!".format(tuple[i]["type"]))
                     continue
+        elif "out" in tuple[i]:
+            value = tuple[i]["out"]
 
         try:
-            value = tuple[i]["def"](value,out)
+            if "def" in tuple[i]:
+                value = tuple[i]["def"](value,out)
             if value != None:
                 print(value)
         except Exception as e:
@@ -140,24 +162,73 @@ def ForTask5(value, out):
     my_list.sort(reverse=True)
     return out.format(my_list)    
 
+# Функции ниже относится к задаче № 6
+goods = list()
+def ForTask6_1(value, out):
+    global goods
+    goods.append((len(goods)+1,{"название":value}))
+
+def ForTask6_DictSet(name,value):
+    global goods
+    total=len(goods)
+    goods[total-1][1][name]=value
+
+def ForTask6_2(value, out):
+    ForTask6_DictSet("цена",value)
+
+def ForTask6_3(value, out):
+    ForTask6_DictSet("количество",value)
+
+def ForTask6_4(value, out):
+    ForTask6_DictSet("ед.изм.",value)
+    global goods
+    return out.format(goods[len(goods)-1])
+
+def ForTask6_5(value, out):
+    if value != 'y' and value != 'yes':
+        return ForTask6_Result()
+
+    global tasks
+    global task_6
+    tasks.append(task_6)
+
+def ForTask6_Result():
+    values = dict()
+    for i in range(len(goods)):
+        for key, value in goods[i][1].items():
+            if not key in values:
+                values[key] = set()
+            values[key].add(value)
+
+    print(values)
+
 # Конфигурируем программу добавляя задачи в список
 # В каждой задаче настраиваем ввод, вывод, исполняющую функцию и тип ожидаемых данных от пользователя
 # Последовательноть выволнения задач будет в соответствии со списком tasks 
 # Если необходимо, то список можно сортировать, так как номера задач весьма условны
 # Сортировка внутри кортежа (одной задачи), недопустима!
-tasks = list()
-#tasks.append(({"def":ForTask1}))
-#tasks.append(({"in":"Введите список, например: [1,2,4,6,3] : ", "out":"Результат = {0}", "def":ForTask2, "type": str}))
-#tasks.append(({"in":"Введите номер месяца: ", "out":"Время года = {0}", "def":ForTask3, "type": int}))
-tasks.append(({"in":"Введите несколько слов: ", "def":ForTask4, "type": str}))
-#tasks.append(({"in":"Введите натуральное число (новый элемент рейтинга): ", "out":"Результат = {0}", "def":ForTask5, "type": int}))
 
+tasks = list()
+tasks.append(({"def":ForTask1}))
+tasks.append(({"in":"Введите список, например: [1,2,4,6,3] : ", "out":"Результат = {0}", "def":ForTask2, "type": str}))
+tasks.append(({"in":"Введите номер месяца: ", "out":"Время года = {0}", "def":ForTask3, "type": int}))
+tasks.append(({"in":"Введите несколько слов: ", "def":ForTask4, "type": str}))
+tasks.append(({"in":"Введите натуральное число (новый элемент рейтинга): ", "out":"Результат = {0}", "def":ForTask5, "type": int}))
+task_6=(
+    ({"out":"Добавление товара в программу..."}),
+    ({"in":"Введите название: ", "def":ForTask6_1, "type": str}),
+    ({"in":"Введите цену: ", "def":ForTask6_2, "type": {int,float}}),
+    ({"in":"Введите количество:", "def":ForTask6_3, "type": {int,float}}),
+    ({"in":"Введите eдицину измерения: ", "out":"Товар добавлен: {0}", "def":ForTask6_4, "type": str}),
+    ({"in":"Введите 'yes' или 'y', чтобы продолжить добавление товаров, а для аналитики нажмите Enter: ", "def":ForTask6_5, "type": str}))
+tasks.append(task_6)    
 
 # Основной цикл
 while True:
     # Основная функция
     main(tasks)
-    if 'y' != input("Введите 'y', чтобы повторить, а для выхода нажмите Enter: "):
+    value = input("Введите 'yes' или 'y', чтобы повторить программу, а для выхода нажмите Enter: ")
+    if value != 'y' and value != 'yes':
         break
 
 print("Завершение программы")
